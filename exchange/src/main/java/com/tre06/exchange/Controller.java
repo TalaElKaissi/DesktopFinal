bd53ac7115f139796e6412f46e04d7fb6f9b63e7
TalaElKaissi <84604689+TalaElKaissi@users.noreply.github.com>
2023-03-08 01:45:19 +0200

    phase 2.1

--- exchange/src/main/java/com/tre06/exchange/Controller.java
+++ exchange/src/main/java/com/tre06/exchange/Controller.java
@@ -1,8 +1,24 @@
 package com.tre06.exchange;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class Controller {
+    public Label buyUsdRateLabel;
+    public Label sellUsdRateLabel;
+    public TextField lbpTextField;
+    public TextField usdTextField;
+    public ToggleGroup transactionType;
+    public void initialize() {
+        fetchRates();
+    }
+    private void fetchRates() {
 
+    }
+    public void addTransaction(ActionEvent actionEvent) {
+
+    }
 }
